e3af8baf05d10f3434186acf091af75b33d9be91
Morley Zhi <morley@lightyear.io>
2019-01-28 11:20:34 -0500

    try upgrading phantomjs

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['mocha', 'sinon-chai'],
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS', 'Firefox'],
     browserNoActivityTimeout: 20000,
 
     files: ['dist/xdr.js', 'test/unit/**/*.js'],
