e2233d2923c314073a6021a03fbd37c04fbaee14
Ryan Yang <ryan.yang@stellar.org>
2026-05-27 12:23:32 -0700

    refactor: replace dynamic xdr construction with static classes

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// Bases (XdrValue, BytesValue, …) and the schema-builder primitives.
+export { XdrError } from './core/error.js';
+
+export { Writer } from './core/writer.js';
+export { Reader } from './core/reader.js';
+
+export type { XdrType } from './core/xdr-type.js';
+export { BaseType } from './core/xdr-type.js';
+
+export { array } from './types/array.js';
+export { bool } from './types/bool.js';
+export { double } from './types/double.js';
+export { enumType } from './types/enum.js';
+export { fixedArray } from './types/fixed-array.js';
+export { float } from './types/float.js';
+export { int32 } from './types/int32.js';
+export { int64 } from './types/int64.js';
+export { lazy } from './types/lazy.js';
+export { opaque } from './types/opaque.js';
+export { option } from './types/option.js';
+export { string } from './types/string.js';
+export { struct } from './types/struct.js';
+export { uint32 } from './types/uint32.js';
+export { uint64 } from './types/uint64.js';
+export { union } from './types/union.js';
+export { varOpaque } from './types/var-opaque.js';
+export { void } from './types/void.js';
